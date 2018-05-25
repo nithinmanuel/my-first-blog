3e4b31837ad053650de35fcd6bd6065c2c836b29
nithin <nithinmanuel9@gmail.com>
2018-05-25 12:18:18 +0200

    modifications on templelates.

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-	author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+	author = models.ForeignKey('auth.User', on_delete=models.CASCADE) # the idea is we have attributies in the left and rught side we have the models.data type
 	title = models.CharField(max_length=200)
 	text = models.TextField()
 	created_date = models.DateTimeField(default=timezone.now)
@@ -16,4 +16,4 @@ class Post(models.Model):
 	def _str_(self):
 		   return self.title  
 
-# Create your models here.
+# Create your models here., in the model we just create model with attributes , the entries are here author,title, text are the attributes., the model named post should be like this 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 def post_list(request):
-	return render(request, 'blog/post_list.html',{})
+	posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html',{'posts':posts}) # this view is to get the data from database  
 
 # Create your views here.
+# the items writting in the views are actions(function) that is need to perform on the data , the data might need to write to data base or to retrive
+# from the data base or to delete, update so on , here we have request , in the request we have the data to arrange. 
